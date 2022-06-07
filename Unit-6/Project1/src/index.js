2733250318c65e5858bcfd41f390645c8a397edd
Parag Tharani <paragtharani24@gmail.com>
2022-06-07 17:17:43 +0530

    Webpack and Babel

--- Unit-6/Project1/src/index.js
+++ Unit-6/Project1/src/index.js
@@ -4,8 +4,30 @@ import ('../Style/index.css')
 
 
 ReactDOM.render(
-    <img src='../logo/logo.png'></img>
-
+    [
+    <img src='../logo/logo.png'></img>,
+    <h1>NotePad</h1>
+    ]
     ,
     document.getElementById("navBar")
 )
+
+ReactDOM.render(
+    [
+        <input type="text" id="noteInput"></input>,
+        <button onClick={displayData}>Enter</button>
+    ],
+    document.getElementById("input")
+)
+
+function displayData(){
+
+    if(document.getElementById("noteInput").value != ""){
+
+    var p = document.createElement("p")
+    p.innerText = document.getElementById("noteInput").value;
+    document.getElementById("noteInput").value = ""
+    
+    document.getElementById("output").append(p);
+    }
+}
