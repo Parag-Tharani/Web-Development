337556c1e6aa4a68dc5149ad71a75924648fa87a
Parag Tharani <paragtharani24@gmail.com>
2022-06-13 02:39:19 +0530

    Grocery Todo list with Json-Server

--- Unit-6/React PC1/profile-todo/src/components/GroceryTodo.jsx
+++ Unit-6/React PC1/profile-todo/src/components/GroceryTodo.jsx
@@ -1,53 +1,72 @@
 import React from "react";
 import { TodoInput } from "./GroceryTodoInput";
 import { TodoList } from "./GroceryTodoList";
-import{ v4 as uuid} from "uuid"
 
 function Todo(){
     
-    const [data, addData] = React.useState([
-        {
-            title: "Eat",
-            status: true,
-            id: 1
-        },
-        {
-            title: "Sleep",
-            status: false,
-            id: uuid()
-        },
-        {
-            title: "Code",
-            status: true,
-            id: uuid()
-        }
-    ]);
+    const [data, addData] = React.useState([]);
+    const [loading, setLoading] = React.useState(false);
+    const [error, setError] = React.useState(false);
+    const [page, setPage] = React.useState(1);
+    const PageLimit = 5
+
+    const fetchData = () => {
+        setLoading(true);
+        fetch(`http://localhost:3001/todos?_page=${page}&_limit=${PageLimit}`)
+        .then((res) => res.json())
+        .then((res) => { addData(res); setLoading(false) })
+        .catch(() => { setError(true); addData([]); setLoading(false)})
+    }
+
+    React.useEffect(() => {
+        fetchData()
+    },[page])
+
+
 
     const handleData = (title,status) => {
 
         const dataFormat = {
             title,
-            status,
-            id: uuid()
+            status
         }
         
         if(title !== ""){
-        addData([...data, dataFormat])
+        fetch(`http://localhost:3001/todos`,{
+            method: "POST",
+            body: JSON.stringify(dataFormat),
+            headers: {
+                "content-type" : "application/json"
+            }
+        })
+        .then((res) => res.json())
+        .then((res) => {return fetchData()})
+        .catch(() => {setError(true); setLoading(false)})
         }
   }
 
   const handleDelete = (id) => {
-      const updataData = data.filter( (items) => items.id !== id);
 
-      addData(updataData);
+    fetch(`http://localhost:3001/todos/${ id }`,{
+        method: "DELETE",
+    })
+    .then((res) => res.json())
+    .then(() => {return fetchData()})
+    .catch(() => {setError(true); setLoading(false)})
   }
 
-
-    return <>
+    return loading ? (<h2> Please Wait <br />Loading ahead...</h2>)
+    : error ? (<h2> Aghh...Something Went Wrong :( </h2>)
+    : ( <>
         <h2>Grocery Todo List</h2>
         <TodoInput handleData={ handleData } />
         <div id="todoList">{ data.map(items => <TodoList key={items.id} {...items} handleDelete={handleDelete} />)}</div>
-    </>
+        <div id="pageShift">
+            <button onClick={() => setPage(page - 1)} disabled={page === 1} >Prev</button>
+            <button onClick={() => setPage(page + 1)} disabled={page === Math.ceil(10/PageLimit)}>Next</button>
+            </div>
+        </>
+        )
 }
 
 export { Todo }
